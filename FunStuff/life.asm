95e96df937e17b21d533312c2ef50192f47e7452
Martin Heermance <mheermance@gmail.com>
2022-06-09 16:06:41 -0400

    wrote macros for loading pointers and comparison.

--- FunStuff/life.asm
+++ FunStuff/life.asm
@@ -1,19 +1,33 @@
 ; -----------------------------------------------------------------------------
-; Game of life but I created it to illustrate my least number of instructions
-; coding challenge. Toward that end I avoid the X and Y registers and their
-; associated instructions, as well as packed decimal.
+; Game of life but created to illustrate my Turing Tarpit coding challenge.
 ; See http://en.wikipedia.org/wiki/Conway's_Game_of_Life
+;
+; The goal of the challege is to use the least number of instructions and
+; towards that end I avoid the X and Y registers, their instructions, as well
+; as packed decimal.
+; 
 ; Martin Heermance <mheermance@gmail.com>
 ; -----------------------------------------------------------------------------
 
 ;
 ; Aliases
 ;
+
+; Character set (ASCII)
+.alias AscBS	$08	; backspace ASCII character
+.alias AscCC	$03	; break (Control-C) ASCII character
+.alias AscCR	$0D	; carriage return ASCII character
+.alias AscDEL	$7F	; DEL ASCII character
+.alias AscESC	$1B	; Escape ASCII character
+.alias AscLF	$0A	; line feed ASCII character
+.alias AscSP	$20	; space ASCII character
+
 .alias height 24	; constants for board height, width, and size.
 .alias width 32
 .alias size height * width
 
 .alias negOne $ffff	; precomputed negative quantities to avoid subtraction
+.alias negSize [0-size]
 .alias negWidth [0-width]
 
 ;
@@ -25,6 +39,7 @@
 .space col 2
 
 .space temp 2		; working variables
+.space strPtr 2		; pointer used for string processing.
 
 .space rowJmpPtr 2	; ponters to functions for iteration.
 .space colJmpPtr 2
@@ -34,8 +49,8 @@
 
 .data BSS
 .org $0300		; page 3 is used for uninitialized data.
-.space gen_curr size	; allocate arrays to hold current and next gens
-.space gen_next size
+.space gen_curr 768	; allocate arrays to hold current and next gens
+.space gen_next 768
 
 .text
 
@@ -54,6 +69,37 @@
         sta _1+1
 .macend
 
+.macro compareConstant
+	lda _1
+	cmp #<_2
+	beq _equal
+
+	lda _1+1		; low bytes are not equal, compare MSB
+	sbc #>_2
+	ora #$01		; Make Zero Flag 0 because we're not equal
+	bvs _overflow
+	bra _notequal
+
+_equal:				; low bytes are equal, so we compare high bytes
+	lda _1+1
+	sbc #>_2
+	bvc _done
+
+_overflow:			; handle overflow because we use signed numbers
+	eor #$80		; complement negative flag
+
+_notequal:
+	ora #$01		; if overflow, we can't be equal
+_done:
+.macend
+	
+.macro loadPtr
+	lda #<_2
+	sta _1
+	lda #>_2
+	sta _1+1
+.macend
+
 .org $8000
 .outfile "life.rom"
 .advance $8000
@@ -61,7 +107,18 @@
 ;
 ; Functions
 ;
+_welcome:
+	.byte "Game of Life",0
+printWelcome:
+	`loadPtr strPtr, _welcome
+	jsr cputs
+	lda #AscLF
+	jsr _putch
+	rts
+
 main:
+	`loadPtr colJmpPtr, printWelcome
+	jsr colForEach
 	brk
 
 ; Sets the row offset to zero
@@ -73,22 +130,13 @@ rowFirst:
 
 ; Advances the offset by the width.
 rowNext:
-	`addConstant row width
+ 	`addConstant row, width
 	rts
 
-; At end if current offset exceeds array size.  
+; At end if current offset exceeds array size.
 rowAtEnd?:
-.scope	
-	lda row
-	cmp #<size
-	beq _else
-	bpl _else
-	lda #$00
+	`compareConstant row, size
 	rts
-_else:
-	lda #$ff
-	rts
-.scend
 
 ; Iterator used to apply a function to the rows.
 rowForEach:
@@ -98,28 +146,28 @@ _loop:
 	jsr _indirectJmp
 	jsr rowNext
 	jsr rowAtEnd?
-	beq _loop
+	bmi _loop
 	rts
 _indirectJmp:
 	jmp (rowJmpPtr)
 .scend
 
 ; Returns index of the row after current using wrap around.
-row+:
+rowPlus:
 	lda row
 	sta temp
 	lda row+1
 	sta temp+1
-	`addConstant temp width
+	`addConstant temp, width
 	rts
 
 ; Returns index of the column before current using wrap around.
-row-:
+rowMinus:
 	lda row
 	sta temp
 	lda row+1
 	sta temp+1
-	`addConstant temp negWidth
+	`addConstant temp, negWidth
 	rts
 
 colFirst:
@@ -129,10 +177,11 @@ colFirst:
 	rts
 
 colNext:
-	`addConstant col 1
+	`addConstant col, 1
+	rts
 
 colAtEnd?:
-	;;     col @ width >=
+	`compareConstant col, width
 	rts
 
 ; Iterator used to apply a function to the cols.
@@ -143,19 +192,19 @@ _loop:
 	jsr _indirectJmp
 	jsr colNext
 	jsr colAtEnd?
-	beq _loop
+	bmi _loop
 	rts
 _indirectJmp:
 	jmp (colJmpPtr)
 .scend
 
 ; Returns index of the column after current using wrap around.
-col+:
+colPlus:
 ;;     col @ 1 + width mod ;
 	rts
 
 ; Returns index of the column before current using wrap around.
-col-:
+colMinus:
 ;;     col @ 1 - width mod ;
 	rts
 
@@ -167,14 +216,11 @@ moveCurr:
 ; clears curr array to clear out junk in ram
 currErase:
 .scope
-	lda #<gen_curr
-	sta genCurrPtr
-	lda #>gen_curr
-	sta genCurrPtr+1
+	`loadPtr genCurrPtr, gen_curr
 _loop:
 	lda #00
 	sta (genCurrPtr)
-	`addConstant genCurrPtr 1
+	`addConstant genCurrPtr, 1
 	rts
 .scend
 
@@ -183,63 +229,72 @@ curr@:
 ;;    + gen_curr + c@ ;
 	rts
 
-\ stores a value into a cell from the current generation
-: curr! ( n col row -- )
-    + gen_curr + c! ;
+; stores a value into a cell from the current generation
+curr!:
+;; 	+ gen_curr + c!
+	rts
 
-\ Parses a pattern string into current board.
-\ This function is unsafe and will over write memory.
-: >curr ( addr count -- )
-    currErase
-    rowFirst colFirst
-    1-
-    for
-        dup c@
-        dup '|' <> if
-            bl <> 1 and
-            col @ row @ curr!
-	    colNext
-	else
-	    drop
-	    rowNext
-	    colFirst
-	then
-	1+
-    next
-    drop ;
+; Parses a pattern string into current board.
+; This function is unsafe and will over write memory.
+currParse:
+	jsr currErase
+	jsr rowFirst
+	jsr colFirst
+;    1-
+;     for
+;        dup c@
+;        dup '|' <> if
+;            bl <> 1 and
+;             col @ row @ curr!
+;	    colNext
+; 	else
+;	    drop
+;	    rowNext
+; 	    colFirst
+;	then
+; 	1+
+;     next
+;     drop ;
+	rts
 
-: .cell ( -- )
-    col @ row @ curr@
-    if '*' else '.' then
-    emit ;
+currCell:
+;    col @ row @ curr@
+;     if '*' else '.' then
+;     emit ;
+	rts
 
-\ prints the row from the current generation to output
-: .currRow ( -- )
-    cr ['] .cell colForEach ;
+; prints the row from the current generation to output
+currRow:
+;	cr ['] .cell colForEach
+	rts
 
-\ Prints the current board generation to standard output
-: .curr
-    ['] .currRow rowForEach
-    cr ;
+; Prints the current board generation to standard output
+curr:
+;     ['] .currRow rowForEach
+;     cr ;
+	rts
 
-\ retrieve a cell value from the next generation
-: next@ ( col row -- n )
-    + gen_next + c@ ;
+; retrieve a cell value from the next generation
+next@:
+;    + gen_next + c@ ;
+	rts
 
-\ stores a cell into the next generation
-: next! ( n col row -- )
-    + gen_next + c! ;
+; stores a cell into the next generation
+next!:
+;    + gen_next + c! ;
+	rts
 
-\ computes the sum of the neigbors of the current cell.
-: calcSum ( -- n )
-   col-  row-  curr@
-   col @ row-  curr@ +
-   col+  row-  curr@ +
-   col-  row @ curr@ +
-   col+  row @ curr@ +
-   col-  row+  curr@ +
-   col @ row+  curr@ +
-   col+  row+  curr@ + ;
+; computes the sum of the neigbors of the current cell.
+calcSum:
+;   col-  row-  curr@
+;   col @ row-  curr@ +
+;   col+  row-  curr@ +
+;   col-  row @ curr@ +
+;   col+  row @ curr@ +
+;   col-  row+  curr@ +
+;   col @ row+  curr@ +
+;   col+  row+  curr@ + ;
+	rts
 
 calcCell:
 	jsr calcSum
@@ -262,26 +317,32 @@ calcCell:
 	rts
 
 calcRow:
-	lda #<calcCell
-	sta cellJmpPtr
-	lda #>calcCell
-	sta cellJmpPtr+1
+	`loadPtr colJmpPtr, calcCell
 	jsr colForEach
 	rts
 
 calcGen:
-	lda #<calcRow
-	sta rowJmpPtr
-	lda #>calcRow
-	sta rowJmpPtr+1
+	`loadPtr rowJmpPtr, calcRow
 	jsr rowForEach
 	jsr moveCurr
 	rts
 
-: life ( -- )
-    page
-    begin calcGen 0 0 at-xy .curr key? until ;
+life:
+;    page
+;     begin calcGen 0 0 at-xy .curr key? until ;
+	rts
 
+; cputs is like the MSDOS console I/O function. It prints a null terminated
+; string to the console using _putch.
+cputs:
+.scope
+_loop:	lda (strPtr)		; get the string via address from zero page
+	beq _exit		; if it is a zero, we quit and leave
+	jsr _putch		; if not, write one character
+	`addConstant strPtr, 1	; get the next byte
+	bra _loop
+_exit:	rts
+.scend
 
 ; conio functions unique to each platform.
 .alias _py65_putc	$f001	; Definitions for the py65mon emulator
